70a5569335a7fe07c07172130d9cca48984ca603
Alvenis Becerra <alvenisj@gmail.com>
2024-07-02 02:38:56 -0500

    Primera versión de sitio web Responsive

---
+++ app.js
@@ -0,0 +1,101 @@
+// FUNCIÓN QUE CARGA LAS ANIMACIONES DE LAS HABILIDADES
+
+function cargarAnimaciones() {
+  $(function () {
+    $(".chart1").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+  $(function () {
+    $(".chart2").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+  $(function () {
+    $(".chart3").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+  $(function () {
+    $(".chart4").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+  $(function () {
+    $(".chart5").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+  $(function () {
+    $(".chart6").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+  $(function () {
+    $(".chart7").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+  $(function () {
+    $(".chart8").easyPieChart({
+      size: 160,
+      barColor: "#ff5b00",
+      scaleLength: 0,
+      lineWidth: 15,
+      tackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+}
+
+cargarAnimaciones();
